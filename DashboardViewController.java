42f65da4c9aa3deb73b78f03541d8f386d810d33
Jacky <32426170+jacquelinesaelee@users.noreply.github.com>
2018-12-05 20:14:05 -0800

    Add files via upload
    Dashboard View and CSS File

--- DashboardViewController.java
+++ DashboardViewController.java
@@ -8,6 +8,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+/*
+    TODO: Object type for User Avatar? Then initialize
+ */
+
 public class DashboardViewController {
 
     @FXML
@@ -15,6 +19,10 @@ public class DashboardViewController {
 
     @FXML
     private Label firstNameLabel;
+
+    @FXML
+    private Label lastNameLabel;
+
 
     @FXML
     private Button settingsButton;
@@ -28,6 +36,14 @@ public class DashboardViewController {
     @FXML
     private Button deleteFriends;
 
+    // Added
+    @FXML
+    private Label ageLabel;
+
+
+
+
+
     @FXML
     void settingsButtonPressed(ActionEvent event) throws IOException {
         Stage stage;
@@ -35,6 +51,7 @@ public class DashboardViewController {
         stage=(Stage) ((Button)(event.getSource())).getScene().getWindow();
         root = FXMLLoader.load(getClass().getResource("SettingsView.fxml"));
         Scene scene = new Scene(root);
+        //scene.getStylesheets().add(getClass().getResource("styles/Font/UserInterface.css").toExternalForm())
         stage.setScene(scene);
         stage.show();
     }
@@ -64,4 +81,13 @@ public class DashboardViewController {
     
 
 }
+
+/*
+    TODO: Fix ERROR when clicking "Login" Button on first page
+    Dec 02, 2018 2:44:41 PM javafx.fxml.FXMLLoader$ValueElement processValue
+    WARNING: Loading FXML document with JavaFX API of version 8.0.171 by JavaFX runtime of version 8.0.131
+    Dec 02, 2018 2:44:41 PM com.sun.javafx.css.parser.CSSParser term
+    WARNING: CSS Error parsing '*{-fx-background-color: #3b5998; -fx-border-color: ;}: Unexpected token ';' at [1,51]
+
+ */
 
